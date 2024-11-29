b29a01d8b5f9b6f8b8c270fef918d526f1a22248
InfTiGames <92449832+InfTiGames@users.noreply.github.com>
2024-11-29 21:51:59 +0100

    Added validation atributes

--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace EShopWeb.Data.Models;
@@ -6,10 +6,22 @@ namespace EShopWeb.Data.Models;
 public class User
 {
     public int UserID { get; set; }
+
+    [Required(ErrorMessage = "Username is required.")]
+    [StringLength(50, ErrorMessage = "Username cannot exceed 50 characters.")]
+    [RegularExpression(@"\S.*", ErrorMessage = "Username cannot be empty or whitespace.")]
     public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [RegularExpression(@"^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Za-z])[A-Za-z\d!@#$%^&*]{8,}$",
+            ErrorMessage = "Password must contain at least one letter, one number, and one special character.")]
     public string PasswordHash { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
